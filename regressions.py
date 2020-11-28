f6af38e0d25fcdcc5cdcbe912c9106c15517e364
Max <30370890+MaxwellJr@users.noreply.github.com>
2020-11-28 20:14:53 +0300

    Update regressions.py

--- regressions.py
+++ regressions.py
@@ -11,24 +11,31 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 dataset = pandas.read_csv('NFLX_5Y.csv')
 dataset = dataset[['Date', 'Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume']]
 dataset = dataset[['Close']]
-forecast_out = 5
-dataset['Prediction'] = dataset['Close'].shift(-forecast_out)
-X = numpy.array(dataset.drop('Prediction', axis=1))[:-forecast_out]
-y = numpy.array(dataset['Prediction'])[:-forecast_out]
+forecast_out = 3
+for i in range(forecast_out):
+    dataset.insert(0, f'Close N-{i + 1}', dataset['Close'].shift(i + 1))
+    dataset[f'Prediction N+{i + 1}'] = dataset['Close'].shift(-i - 1)
+dataset = dataset[forecast_out:-forecast_out]
+dataset.reset_index(drop=True, inplace=True)
+print(dataset)
+X = numpy.array(dataset.drop([f'Prediction N+{i + 1}' for i in range(forecast_out)], axis=1))
+y = numpy.array(dataset[[f'Prediction N+{i + 1}' for i in range(forecast_out)]])
 
 # Fit the models
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None, shuffle=False, stratify=None)
 lr_model = LinearRegression(fit_intercept=True, n_jobs=-1)
 lr_model.fit(X_train, y_train)
-X_future = dataset.drop('Prediction', axis=1)[:-forecast_out]
-X_future = X_future.tail(forecast_out)
-X_future = numpy.array(X_future)
-real_future_data = numpy.array(dataset.drop('Prediction', axis=1)[-forecast_out:])
-poly = PolynomialFeatures(degree=3)
-X_poly = poly.fit_transform(X)
+X_future = numpy.array(X[-1:])
+print('X_future')
+print(X_future)
+real_future_data = numpy.array(y[-1:])
+print('Real future data')
+print(real_future_data)
+poly = PolynomialFeatures(degree=2)
+X_poly = poly.fit_transform(X_train)
 X_poly_test = poly.fit_transform(X_future)
 poly_model = LinearRegression()
-poly_model.fit(X_poly, y)
+poly_model.fit(X_poly, y_train)
 
 # Prediction
 lr_prediction = lr_model.predict(X_future)
@@ -47,16 +54,16 @@ print('[Polynomial] Mean Squared Error:', mean_squared_error(poly_prediction, re
 print('[Polynomial] Root Mean Squared Error:', mean_squared_error(poly_prediction, real_future_data, squared=False))
 
 # Plot
-plot_data = dataset[X.shape[0]:]
-plot_data.rename({"Prediction": "Linear prediction"}, axis=1, inplace=True)
-plot_data['Linear prediction'] = lr_prediction
-plot_data['Poly prediction'] = poly_prediction
+plot_data = pandas.DataFrame({'Prediction' : numpy.insert(real_future_data[0], 0, X_future[0][-1])})
+plot_data.index += X.shape[0] - 1
+plot_data.insert(1, 'Linear prediction', numpy.insert(lr_prediction[0], 0, X_future[0][-1]))
+plot_data.insert(2, 'Poly prediction', numpy.insert(poly_prediction[0], 0, X_future[0][-1]))
 print(plot_data)
 pyplot.figure(figsize=(8, 4))
 pyplot.title('Model')
 pyplot.xlabel('Days')
 pyplot.ylabel('Close Price')
 pyplot.plot(dataset['Close'])
-pyplot.plot(plot_data[['Linear prediction', 'Poly prediction']])
-pyplot.legend(['Real data', 'Linear prediction', 'Poly prediction'])
+pyplot.plot(plot_data[['Prediction', 'Linear prediction', 'Poly prediction']], marker=".")
+pyplot.legend(['Historical data', 'Real Values', 'Linear prediction', 'Poly prediction'])
 pyplot.show()
